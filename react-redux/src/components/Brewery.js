2325e7154089728138465a3bf996964b50f65b82
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-10-10 17:32:25 -0400

    Continue styling

--- react-redux/src/components/Brewery.js
+++ react-redux/src/components/Brewery.js
@@ -4,8 +4,8 @@ const Brewery = props => {
     console.log(props)
     return (
         <div className='brewery'>
-            <h1>{props.brewery.name}</h1>
-            <h5>{props.brewery.brewery_type.toUpperCase()}</h5>
+            <a href={props.brewery.website_url} target='_blank' rel='noopener noreferrer'><h1>{props.brewery.name}</h1></a>
+            <h5>{props.brewery.brewery_type}</h5>
             <p>{props.brewery.city}, {props.brewery.state}</p>
         </div>
     )
